96bc1399e3a294569066824f02a78658712f69c6
Wasealex <wasealex27@gmail.com>
2022-07-14 13:35:22 +0000

    main.c

--- main.c
+++ main.c
@@ -1,48 +1,91 @@
+#define  _POSIX_C_SOURCE 200809L
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include "monty.h"
-/*global struct*/
-/*monty_details mon;*/
-#define AGSIZE 10
+
+void file_error(char *argv);
+void error_usage(void);
+int status = 0;/* global var declaration */
+
 /**
- *main - main file of the monty interpreter
- *@argc:arguement count
- *@argv:arguement vector
- *Return: 0
+ *main - entry point
+ *@argv: list of arguments passed to our program
+ *@argc: amount of args
+ *
+ *Return: nothing
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	/*initialization of mon variables*/
-	mon.line_number = 1;
-	mon.stack_queue = 1;
-	mon.file = NULL;
-	mon.args = malloc(sizeof(char *) * AGSIZE);
-	if (!mon.args)
-	{
-		dprintf(STDERR_FILENO, "Error: malloc failed\n");
-		freer();
-		exit(EXIT_FAILURE);
-	}
-	/*arguement count should be 2*/
+	FILE *file;
+	size_t buf_len = 0;
+	char *buffer = NULL;
+	char *str = NULL;
+	stack_t *stack = NULL;
+	unsigned int line_cnt = 1;
+
+	global.data_struct = 1;  /* struct defined in monty.h L58*/
 	if (argc != 2)
+		error_usage(); /* def in line 82 */
+
+	file = fopen(argv[1], "r");
+
+	if (!file)
+		file_error(argv[1]);  /* def in line 68 */
+
+	while ((getline(&buffer, &buf_len, file)) != (-1))
 	{
-		dprintf(STDERR_FILENO, "USAGE: monty file\n");
-		freer();
-		exit(EXIT_FAILURE);
+		if (status)
+			break;
+		if (*buffer == '\n')
+		{
+			line_cnt++;
+			continue;
+
+		}
+		str = strtok(buffer, " \t\n");
+		if (!str || *str == '#')
+		{
+			line_cnt++;
+			continue;
+
+		}
+		global.argument = strtok(NULL, " \t\n");
+		opcode(&stack, str, line_cnt);
+		line_cnt++;
+
 	}
-	/*check whether file is accessible*/
-	if (access(argv[1], R_OK) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", argv[1]);
-		freer();
-		exit(EXIT_FAILURE);
-	}
-	mon.file = fopen(argv[1], "r");
-	if (!mon.file)
-	{
-		dprintf(STDERR_FILENO, "Error: malloc failed\n");
-		freer();
-		exit(EXIT_FAILURE);
-	}
-	parse();
-	freer();
-	return (EXIT_SUCCESS);
+	free(buffer);
+	free_stack(stack);
+	fclose(file);
+	exit(EXIT_SUCCESS);
+
+}
+
+/**
+ *file_error - prints file error message and exits
+ *@argv: argv given by main()
+ *
+ *Desc: print msg if  not possible to open the file
+ *Return: nothing
+ */
+void file_error(char *argv)
+{
+	fprintf(stderr, "Error: Can't open file %s\n", argv);
+	exit(EXIT_FAILURE);
+
+}
+
+/**
+ *error_usage - prints usage message and exits
+ *
+ *Desc: if user does not give any file or more than
+ *one argument to your program
+ *
+ *Return: nothing
+ */
+void error_usage(void)
+{
+	fprintf(stderr, "USAGE: monty file\n");
+	exit(EXIT_FAILURE);
 }
